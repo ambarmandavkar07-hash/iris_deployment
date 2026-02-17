95efffb84f12bd62304e407ffc52f4d99802312e
ambarmandavkar07-hash <ambarmandavkar07@gmail.com>
2026-02-17 14:03:20 +0530

    Fix syntax errors in app.py

--- app.py
+++ app.py
@@ -18,10 +18,11 @@ petal_length = st.number_input("Petal Length",min_value=1.0, max_value=7.0)
 petal_width = st.number_input("Petal Length",min_value=0.1, max_value=3.0)
 
 # Predict button
-if st,button("Predict"):
+if st.button("Predict"):
     prediction = model.predict([[sepal_length, sepal_width, petal_length ,petal_width]])
     classes = ["Setosa", "Versicolor", "Virginica"]
     st.write(f"Prediction" {classes[prediction[0]]})
+
 
 
 
